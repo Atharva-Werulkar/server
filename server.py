96d6293e91fd089b5adc24aa889cc08b6ce7cb7a
sanketv010 <sanketverma425@gmail.com>
2024-02-10 22:44:29 +0530

    Added Prediction and the ML Model

--- server.py
+++ server.py
@@ -23,6 +23,9 @@
 
 
 from flask import Flask, request, jsonify
+import joblib
+
+xgb = joblib.load('xgboost_model.pkl')
 
 app = Flask(__name__)
 
@@ -31,7 +34,7 @@ def predict():
     try:
         data = request.json['data']
         # Process the data (perform prediction, etc.)
-        prediction = "Your prediction"  # Replace this with your actual prediction logic
+        prediction = xgb.predict(data)  # Replace this with your actual prediction logic
         return jsonify({'prediction': prediction}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 400

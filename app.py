cf13097b8f2c3642c358e72aa427156a84adc615
Atharva Werulkar <werulkaratharva@gmail.com>
2024-02-11 03:07:39 +0530

    Updated

--- app.py
+++ app.py
@@ -1,49 +1,4 @@
 
-# from flask import Flask, request, jsonify
-# import joblib
-
-# xgb = joblib.load('xgboost_model.pkl')
-
-# app = Flask(__name__)
-
-# @app.route('/', methods=['POST'])
-# def predict():
-#     try:
-#         data = request.json['data']
-#         # Process the data (perform prediction, etc.)
-#         prediction = xgb.predict(data)  # Replace this with your actual prediction logic
-#         return jsonify({'prediction': prediction}), 200
-#     except Exception as e:
-#         return jsonify({'error': str(e)}), 400
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# from flask import Flask, request, jsonify
-# import joblib
-# import numpy as np
-
-# xgb = joblib.load('xgboost_model.pkl')
-
-# app = Flask(__name__)
-
-# @app.route('/', methods=['POST'])
-# def predict():
-#     try:
-#         data = request.json['data']
-#         # Process the data (perform prediction, etc.)
-#         prediction = xgb.predict(data)  # Replace this with your actual prediction logic
-#         # Convert NumPy array to list
-#         prediction = prediction.tolist()
-#         if isinstance(prediction[0], np.ndarray):
-#             prediction = [p.tolist() for p in prediction]
-#         return jsonify({'prediction': prediction}), 200
-#     except Exception as e:
-#         return jsonify({'error': str(e)}), 400
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np

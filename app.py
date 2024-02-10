ee12d48095c390c84619604911b0185db4c094cc
Atharva Werulkar <werulkaratharva@gmail.com>
2024-02-10 23:49:53 +0530

    added the requirements

---
+++ app.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, request, jsonify
+import joblib
+
+xgb = joblib.load('xgboost_model.pkl')
+
+app = Flask(__name__)
+
+@app.route('/a', methods=['POST'])
+def predict():
+    try:
+        data = request.json['data']
+        # Process the data (perform prediction, etc.)
+        prediction = xgb.predict(data)  # Replace this with your actual prediction logic
+        return jsonify({'prediction': prediction}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
